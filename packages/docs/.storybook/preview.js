9285810d04e094a7342628fe4ccd435bd4d021b0
Saulo Braga <saulo.braga@live.com>
2023-02-25 00:35:35 -0300

    finalizado configuração do projeto monorepo

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,14 @@
+import { themes } from "@storybook/theming";
+export const parameters = {
+  layout: "centered",
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+};
